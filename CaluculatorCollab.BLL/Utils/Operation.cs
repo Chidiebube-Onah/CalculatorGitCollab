bfab3259bea9f3e07f09c209ff86ef8ffcdf65de
KellynCodes <kellyrobot247@gmail.com>
2023-03-12 01:58:58 +0100

    feat: XML comment for method descriptions

--- CaluculatorCollab.BLL/Utils/Operation.cs
+++ CaluculatorCollab.BLL/Utils/Operation.cs
@@ -1,6 +1,9 @@
 namespace CalculatorCollab.BLL.Utils{
     public class Operation
     {
+        ///<summary>
+        ///Accepts yes or no input from the user to quit the app or continue with operations.
+        ///</summary>
         public static virtual (bool isYes, string msg) ContinueOrQuit(string answer){
             string userAnswer = answer.Trim().ToUpper();
             if(userAnswer == "YES") return (true, $"{answer}");
