6949f3a1ea9ba5fb5ad64f75d289b1b61c2cffb9
Kelechi Amos <kellyrobot247@gmail.com>
2023-03-12 14:57:57 +0100

    Feature/uivalidations (#2)
    * feat: string and number validation, logout check
    
    * feat: XML comment for method descriptions

---
+++ CaluculatorCollab.BLL/Utils/Operation.cs
@@ -0,0 +1,16 @@
+namespace CalculatorCollab.BLL.Utils{
+    public class Operation
+    {
+        ///<summary>
+        ///Accepts yes or no input from the user to quit the app or continue with operations.
+        ///</summary>
+        public static virtual (bool isYes, string msg) ContinueOrQuit(string answer){
+            string userAnswer = answer.Trim().ToUpper();
+            if(userAnswer == "YES") return (true, $"{answer}");
+
+            if(userAnswer == "NO") return (false, $"{answer}");
+
+            if(!userAnswer == "YES" || "NO") return (false, $"{answer} is not a valid choice.");
+        } 
+    }
+}
