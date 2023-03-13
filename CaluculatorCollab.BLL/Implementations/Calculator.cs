65f0b42c2c155e82db253e42db8ebc3520089058
Chelseaquiin <60669974+Chelseaquiin@users.noreply.github.com>
2023-03-13 13:45:42 +0100

    feature: Implemented AddAsync and MultiplyAsync functionalities (#7)

--- CaluculatorCollab.BLL/Implementations/Calculator.cs
+++ CaluculatorCollab.BLL/Implementations/Calculator.cs
@@ -1,42 +1,37 @@
-﻿using CaluculatorCollab.BLL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CaluculatorCollab.BLL.Implementations;
 
-namespace CaluculatorCollab.BLL.Implementations
+public class Calculator : ICalculator
 {
-    public class Calculator : ICalculator
+
+    public async Task<int> AddAsync(int a, int b)
     {
-        public async Task<int> AddAsync(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
+        return a + b;
 
-        public async Task<int> DivideAsync(int number, int divisor)
-        {
-            throw new NotImplementedException();
-        }
+    }
 
-        public async Task<int> ExponentialAsync(int number, int power)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<int> DivideAsync(int number, int divisor)
+    {
+        throw new NotImplementedException();
+    }
 
-        public async Task<int> ModuloAsync(int number, int mod)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<int> ExponentialAsync(int number, int power)
+    {
+        throw new NotImplementedException();
+    }
 
-        public async Task<int> MultiplyAsync(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<int> ModuloAsync(int number, int mod)
+    {
+        throw new NotImplementedException();
+    }
 
-        public async Task<int> SubtractAsync(int a, int b)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<int> MultiplyAsync(int a, int b)
+    {
+        return a * b;
+
+    }
+
+    public async Task<int> SubtractAsync(int a, int b)
+    {
+        throw new NotImplementedException();
     }
 }
