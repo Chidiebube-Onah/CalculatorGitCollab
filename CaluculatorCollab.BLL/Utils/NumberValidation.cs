6a4a74f6f65a81d3160fc08dfd4b8a363c8f1d1e
KellynCodes <kellyrobot247@gmail.com>
2023-03-13 11:58:42 +0100

    fix: method return error and file extension of NumberValidation cLass

--- CaluculatorCollab.BLL/Utils/NumberValidation.cs
+++ CaluculatorCollab.BLL/Utils/NumberValidation.cs
@@ -1,8 +1,6 @@
-using System;
-
 namespace CalculatorCollab.BLL.Utils
 {
-   public class NumberValidation
+   public static class NumberValidation
 {
     ///<summary>
     ///Checks if a number is an even number.
@@ -13,10 +11,10 @@ namespace CalculatorCollab.BLL.Utils
     }
 
 ///<summary>
-///Checks if a string is convertible to number (decimal; for high number inputs)
+///Checks if a string is convertible to number (decimal; for high number inputs).
 ///</summary>
   public static bool IsNumber(string number){
-bool isNumber = decimal.TryParse(number, out number);
+bool isNumber = decimal.TryParse(number, out decimal Convertednumber);
 
 if (isNumber) return true;
 return false;
@@ -26,18 +24,12 @@ return false;
 ///This method checks if the passed number is zero. with default error msg.
 ///</summary>
      public static (bool isZero, string msg) DivideByZeroHandler(int number){
-        bool isValidNum = IsNumber(number);
-        if(isValidNum){
         if(number <= 0) return (true, $"{number} is greater zero.");
         return (false, $"{number} is less than or equal to zero.");
-        }else{
-            return (false, $"{number} is not valid.");
-        }
      }
 
 ///<summary>
 /// You can use this method to check if input is empty and access the validation message. Return  a tuple (true: boolean, with a success message) and (false: boolean, error message) when it is not.
-
 ///</summary>
     public static (bool isIt, string message) IsBlank(this string stringInput)
     {
